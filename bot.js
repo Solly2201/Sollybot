f5df0c55e11b7bae87ba51da099885d8aa5b474d
sollywood <50542158+sollywood@users.noreply.github.com>
2019-05-12 14:30:41 +0530

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,36 @@
+worker: node bot.js
+const commando = require('discord.js-commando');
+const discord = require('discord.js')
+const client = new commando.Client({
+commandPrefix: "++",
+owner: ['474516081059430431'],
+nonCommandEditable: true,
+disableEveryone: true,
+unknownCommandResponse: false,
+invite: "https://discord.gg/eshZx98"})
+var db = require('quick.db')
+client.registry.registerGroup('fun', 'fun');
+client.registry.registerGroup('moderation', 'moderation');
+client.registry.registerGroup('general', 'general');
+client.registry.registerGroup('event', 'event');
+client.registry.registerDefaults();
+client.registry.registerCommandsIn(__dirname + '/Commands');
+global.currentTeamMembers = [];
+global.servers = {};
+
+client.on('ready', () => {
+  client.user.setActivity("++help!!", {type: "Watching"})
+  console.log('I am ready daddy.. Use me now!!!');
+});
+client.on('message', message => {
+  let color = ((1 << 24) * Math.random() | 0).toString(16);
+if (message.content === '<@546265612339511296>') {
+  let pingembed = new discord.RichEmbed()
+   .setColor(`#${color}`)
+  .setTitle(message.author.username)
+  .setDescription('I am SollyBot and I am made by <@474516081059430431>, My default prefix is `++(customizable)`. To know my prefix for this server do <@546265612339511296> prefix and do `++help` to get a list of my commands.')
+  message.channel.send(pingembed)
+}
+  
+});
+client.login(process.env.BOT_TOKEN);
